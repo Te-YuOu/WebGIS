59eef48b1ad8bd6265d273a353516ae2cd35e733
ODY <r13229012@ntu.edu.tw>
2025-09-02 12:31:31 +0800

    Replace script.js with 4-season GeoTIFF version

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
-const map = L.map('map').setView([23.5, 121], 7); // 台灣中心
 
+// 初始化地圖
+const map = L.map('map').setView([23.5, 121], 7);
+
+// OSM 底圖
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-fetch('data/cloud_fog.geojson')
-  .then(response => response.json())
-  .then(data => {
-    const geojson = L.geoJSON(data, {
-      onEachFeature: function (feature, layer) {
-        const props = feature.properties;
-        let content = "";
-        for (const key in props) {
-          content += `<b>${key}</b>: ${props[key]}<br>`;
-        }
-        layer.bindPopup(content);
-      },
-      pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-          radius: 6,
-          fillColor: "#FF5733",
-          color: "#fff",
-          weight: 1,
-          opacity: 1,
-          fillOpacity: 0.8
-        });
-      }
-    }).addTo(map);
-  });
+// 建立四季的 RR GeoTIFF 圖層
+const rrLayerDJF = new L.LeafletGeotiff('./data/RR_map_DJF.tif', {
+  renderer: new L.LeafletGeotiff.Plotty({
+    displayMin: 0.0,
+    displayMax: 0.6,
+    colorScale: 'viridis',
+  }),
+  name: "DJF RR"
+});
+
+const rrLayerMAM = new L.LeafletGeotiff('./data/RR_map_MAM.tif', {
+  renderer: new L.LeafletGeotiff.Plotty({
+    displayMin: 0.0,
+    displayMax: 0.6,
+    colorScale: 'viridis',
+  }),
+  name: "MAM RR"
+});
+
+const rrLayerJJA = new L.LeafletGeotiff('./data/RR_map_JJA.tif', {
+  renderer: new L.LeafletGeotiff.Plotty({
+    displayMin: 0.0,
+    displayMax: 0.6,
+    colorScale: 'viridis',
+  }),
+  name: "JJA RR"
+});
+
+const rrLayerSON = new L.LeafletGeotiff('./data/RR_map_SON.tif', {
+  renderer: new L.LeafletGeotiff.Plotty({
+    displayMin: 0.0,
+    displayMax: 0.6,
+    colorScale: 'viridis',
+  }),
+  name: "SON RR"
+});
+
+// 加入圖層切換控制器
+const overlayMaps = {
+  "DJF": rrLayerDJF,
+  "MAM": rrLayerMAM,
+  "JJA": rrLayerJJA,
+  "SON": rrLayerSON,
+};
+
+L.control.layers({}, overlayMaps, { collapsed: false }).addTo(map);
+
+// 預設顯示 DJF 圖層
+rrLayerDJF.addTo(map);
